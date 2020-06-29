40406fb196f716e9d9a0c5f20a848f4600a7fc33
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-06-29 13:20:03 +0500

    Initial Commit

---
+++ locationToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  locationToDo
+//
+//  Created by Damir Lutfullin on 29.06.2020.
+//  Copyright © 2020 DamirLutfullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
